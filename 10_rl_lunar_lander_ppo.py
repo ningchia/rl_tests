affa6c0c8d622f70f4f76ee73377801c30163e59
ningchia <ningchia123466@gmail.com>
2026-03-27 18:07:59 +0800

    use small batch, fix batch size matching and NaN issues

--- 10_rl_lunar_lander_ppo.py
+++ 10_rl_lunar_lander_ppo.py
@@ -91,6 +91,16 @@ import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 import datetime
 
+ENV_NAME = "LunarLanderContinuous-v3"
+MAX_EPISODES = 1000
+UPDATE_TIMESTEP = 2000 # 每累積 2000 步更新一次
+
+MODEL_SAVE_PATH = "dqn_model"
+CHECKPOINT_FILE = "ppo_lunar_lander.pth"
+
+os.makedirs(MODEL_SAVE_PATH, exist_ok=True)
+checkpoint_path = os.path.join(MODEL_SAVE_PATH, CHECKPOINT_FILE)
+
 # --- PPO 網路架構 ---
 class PPOModel(nn.Module):
     def __init__(self, state_dim, action_dim):
@@ -109,6 +119,8 @@ class PPOModel(nn.Module):
         # 第二維度 (Side Engines)：控制水平方向噴力。數值範圍：[-1, 1]。
         # 負數代表向右噴（推力向左），正數代表向左噴（推力向右）。
         self.actor_mu = nn.Linear(256, action_dim)      # head0: 輸出動作設定值的均值 (mu), 形狀: [batch_size, action_dim]
+        nn.init.constant_(self.actor_mu.bias, 0.5)      # 初始 bias 為 0.5 來強迫初期 mu 偏向正值（噴火）
+
         self.actor_sigma = nn.Linear(256, action_dim)   # head1: 輸出動作設定值的標準差 (sigma), 形狀: [batch_size, action_dim]
         
         # Critic: 輸出預期從目前狀態到終局的累計(打折後的)狀態價值 (V-value)
@@ -117,7 +129,17 @@ class PPOModel(nn.Module):
     def forward(self, state):
         x = self.fc(state)
         mu = torch.tanh(self.actor_mu(x))               # 將head0的輸出(mu)限制在 [-1, 1]
-        sigma = F.softplus(self.actor_sigma(x)) + 1e-5  # 將head1的輸出(sigma)限制在 [0, ∞)
+
+        # sigma = F.softplus(self.actor_sigma(x)) + 1e-5  # 將head1的輸出(sigma)限制在 [0, ∞)
+
+        # 以下這個做法發生過訓練過程中 sigma 為 NaN 的情況. 意思是某些權重在更新後變得NaN, 以至於 sigma 的輸出也變成NaN.
+        # sigma = torch.exp(self.actor_sigma(x) - 0.5) + 1e-5  # 將head1的輸出(sigma) = e^(x - 0.5) 讓初始 sigma 大約在 e^-0.5, 0.6 左右
+        
+        # 將 log_sigma 限制在合理範圍（例如 -20 到 2）
+        # 這能確保 sigma 永遠在 [exp(-20), exp(2)] 之間，不會是 0 也不會太大
+        log_sigma = torch.clamp(self.actor_sigma(x), -20, 2)
+        sigma = torch.exp(log_sigma)
+
         value = self.critic(x)
         return mu, sigma, value
 
@@ -231,7 +253,7 @@ class PPOAgent:
     #      dist_entropy 是動作分佈的熵，而高斯分佈的熵正比於sigma. 熵越大表示動作越多樣化。
     #      而我們希望最大化熵，所以在損失函數中加上負號。
     
-    def update(self, storage):
+    def update(self, storage, timestep):
         # 計算回報 (Rewards-to-go)
         rewards = []
         discounted_reward = 0
@@ -262,6 +284,10 @@ class PPOAgent:
             discounted_reward = reward + (self.gamma * discounted_reward)
             rewards.insert(0, discounted_reward)
         
+        # --- 注意batch_size 匹配 ---
+        # 注意 old_states, old_actions, old_logprobs, old_values, target_values, advantages 
+        # 的 batch_size 都是 UPDATE_TIMESTEP = 2000
+
         # 轉換為 Tensor
         # torch.stack 是將列表中的 Tensor 沿著新的維度堆疊起來，形成一個新的 Tensor。
         # 這裡的 storage.states, storage.actions, storage.logprobs, storage.values 都是列表，每個元素都是一個 Tensor。
@@ -290,13 +316,13 @@ class PPOAgent:
         #    view() 的使用限制
         #        數據連續性：.view() 只能用於「連續的」（contiguous）張量。
         #        如果張量經過 transpose() 或 permute() 等操作，必須先呼叫 .contiguous() 才能使用 .view()，否則會報錯。
-        
+
         # 這裡的 target_values 是 PPO 更新過程中的「真實累計得分」(來自environment的reward, 不是policy預測出來的)，
         target_values = torch.FloatTensor(rewards).to(self.device)      # 形狀: [batch_size]
         
         # 優勢函數 (Advantage). 優勢 (A) = 真實累計得分 - 預期累計得分。
         advantages = target_values - old_values                         # 形狀: [batch_size]
-        advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)   # normalize advantages
+        # advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)   # normalize advantages
 
         # 用來記錄這一輪 10 個 Epoch 的平均數值
         sum_loss = 0
@@ -304,49 +330,84 @@ class PPOAgent:
         sum_value_loss = 0
         sum_entropy = 0
         sum_advantages = 0
+        loss_calc_count = 0
+
+        # 將數據打亂並分成 mini-batches
+        batch_size = 64
+        indices = np.arange(UPDATE_TIMESTEP)  # UPDATE_TIMESTEP 是這一輪收集的總步數，等於 old_states 的第一維度大小。
 
         for _ in range(self.K_epochs):      # K_epochs 是每次 PPO 更新的迭代次數，通常是 10。
-            # 在每次更新迭代中，使用正在訓練的 policy 網路 (self.policy) 計算當前狀態下的動作分佈 (mu, sigma) 
-            # 和狀態價值 (values)。
-            mu, sigma, values = self.policy(old_states)
-            dist = Normal(mu, sigma)
-            # 我們的動作有兩個維度(主引擎、側引擎). sum(dim=-1) 是把 log機率 加總起來，得到整個「決策」的總 log機率。 
-            logprobs = dist.log_prob(old_actions).sum(dim=-1)           # 形狀: [batch_size, action_dim] -> [batch_size]
-            # 每個維度都有自己的 sigma，也就有自己的熵。把這兩個動作維度的不確定性加總起來，得到這整個「決策」的總不確定性。
-            dist_entropy = dist.entropy().sum(dim=-1)                   # 形狀: [batch_size, action_dim] -> [batch_size]
-            
-            # PPO 核心：計算 Ratio (pi_theta / pi_theta_old), 代表新policy對此action設定值的偏好程度"趨勢".
-            ratios = torch.exp(logprobs - old_logprobs)                 # 形狀: [batch_size]
 
-            # Clipped Surrogate Objective
-            surr1 = ratios * advantages                                 # 形狀: [batch_size]
-            surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip) * advantages
-            # PPO 的損失函數包含：
-            #   Policy Loss = -torch.min(surr1, surr2) ：加負號是因為我們要「最大化」優點。
-            #   Value Loss = 0.5 * MSELoss             ：這是 。訓練 Critic（評論家）讓它預測得分越來越準。
-            #   混亂獎勵 = -0.01 * Entropy              ：獎勵那些 sigma 比較大的行為，防止 AI 太快變死板。
-            # PPO的訓練目標: 
-            #   Advantage 慢慢趨近於 0，但 Reward 卻很高.
-            #   那就代表 Critic 已經變成了「預言家」，而 Actor 已經變成了「頂尖飛行員」。
-            policy_loss = -torch.min(surr1, surr2).mean()                       # 這邊的mean是在batch維度上.
-            value_loss = 0.5 * self.mse_loss(values.squeeze(), target_values)   # MSELoss 會自動在batch維度上取 mean.
-            entropy_loss = -0.01 * dist_entropy.mean()                          # 這邊的mean是在batch維度上.
-            # 總損失 (取mean過的)
-            loss = policy_loss + value_loss + entropy_loss            
+            np.random.shuffle(indices)
+            for start in range(0, UPDATE_TIMESTEP, batch_size):
+                end = start + batch_size
+                idx = indices[start:end]
 
-            self.optimizer.zero_grad()
-            loss.backward()
-            # 梯度裁剪 (Gradient Clipping) 是一種常用的技巧，用於防止梯度爆炸（Gradient Explosion）問題.
-            # 0.5 是裁剪的閾值，表示如果梯度的 L2 範數超過 0.5，就會被縮放回 0.5。
-            nn.utils.clip_grad_norm_(self.policy.parameters(), 0.5)     
-            self.optimizer.step()
+                loss_calc_count += 1
+                
+                # --- 注意batch_size 匹配 ---
+                # 注意這邊 mu, sigma, values, dist, logprob, dist_entropy, ratios, surr1, surr2, 
+                # policy_loss, value_loss, entropy_loss, loss 的 batch_size 都是 64.
 
-            # 累加數值
-            sum_loss += loss.item()
-            sum_policy_loss += policy_loss.item()
-            sum_value_loss += value_loss.item()
-            sum_entropy += dist_entropy.mean().item()           
-            sum_advantages += advantages.mean().item() 
+                # 只取一部分樣本更新，能讓梯度更具方向性
+                mu, sigma, values = self.policy(old_states[idx])    
+
+                # 在每次更新迭代中，使用正在訓練的 policy 網路 (self.policy) 計算當前狀態下的動作分佈 (mu, sigma) 
+                # 和狀態價值 (values)。
+                # 注意這邊 dist 的 batch_size = 64.
+                dist = Normal(mu, sigma)
+                # 我們的動作有兩個維度(主引擎、側引擎). sum(dim=-1) 是把 log機率 加總起來，得到整個「決策」的總 log機率。 
+                # 注意這邊不能直接用old_actions, 得用 old_actions[idx], batch_size 才會match = 64.
+                logprobs = dist.log_prob(old_actions[idx]).sum(dim=-1)      # 形狀: [batch_size, action_dim] -> [batch_size]
+                # 每個維度都有自己的 sigma，也就有自己的熵。把這兩個動作維度的不確定性加總起來，得到這整個「決策」的總不確定性。
+                dist_entropy = dist.entropy().sum(dim=-1)                   # 形狀: [batch_size, action_dim] -> [batch_size]
+                
+                # PPO 核心：計算 Ratio (pi_theta / pi_theta_old), 代表新policy對此action設定值的偏好程度"趨勢".
+                # 注意這邊不能直接用old_logprobs, 得用 old_logprobs[idx], batch_size 才會match = 64.
+                ratios = torch.exp(logprobs - old_logprobs[idx])            # 形狀: [batch_size]
+
+                # ratios 是 tensor. 
+                # torch.isnan(ratios) 的輸出是 同樣形狀的布林 tensor。如果沒有.any()，那麼即使有 NaN 也不會被檢查出來。
+                # .any() 是用來檢查 Tensor 中是否存在任何元素為 NaN（Not a Number）。如果 ratios 中有任何一個元素是 NaN，這個條件就會返回 True。
+                if torch.isnan(ratios).any():       
+                    print(f"發現 ratios 含有 NaN Loss。\n idx: {idx} \n logprobs: {logprobs}\n old_logprobs[idx]: {old_logprobs[idx]}")
+                    exit()
+
+                # Clipped Surrogate Objective
+                surr1 = ratios * advantages[idx]                            # 形狀: [batch_size]
+                surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip) * advantages[idx]
+                # PPO 的損失函數包含：
+                #   Policy Loss = -torch.min(surr1, surr2) ：加負號是因為我們要「最大化」優點。
+                #   Value Loss = 0.5 * MSELoss             ：這是 。訓練 Critic（評論家）讓它預測得分越來越準。
+                #   混亂獎勵 = -0.01 * Entropy              ：獎勵那些 sigma 比較大的行為，防止 AI 太快變死板。
+                # PPO的訓練目標: 
+                #   Advantage 慢慢趨近於 0，但 Reward 卻很高.
+                #   那就代表 Critic 已經變成了「預言家」，而 Actor 已經變成了「頂尖飛行員」。
+                policy_loss = -torch.min(surr1, surr2).mean()                           # 這邊的mean是在batch維度上.
+                value_loss = 0.5 * self.mse_loss(values.squeeze(), target_values[idx])  # MSELoss 會自動在batch維度上取 mean.
+                entropy_loss = -0.01 * dist_entropy.mean()                              # 這邊的mean是在batch維度上.
+                # 總損失 (取mean過的)
+                loss = policy_loss + value_loss + entropy_loss            
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                # 梯度裁剪 (Gradient Clipping) 是一種常用的技巧，用於防止梯度爆炸（Gradient Explosion）問題.
+                # 0.5 是裁剪的閾值，表示如果梯度的 L2 範數超過 0.5，就會被縮放回 0.5。
+                nn.utils.clip_grad_norm_(self.policy.parameters(), 0.5)     
+                self.optimizer.step()
+
+                # 累加數值
+                sum_loss += loss.item()
+                sum_policy_loss += policy_loss.item()
+                sum_value_loss += value_loss.item()
+                sum_entropy += dist_entropy.mean().item()           
+                sum_advantages += advantages[idx].mean().item() 
+
+        # 記錄 Actor 的輸出統計
+        writer.add_scalar('Stats/Mu_Main', mu[:, 0].mean().item(), timestep)   # 主引擎均值
+        writer.add_scalar('Stats/Sigma_Main', sigma[:, 0].mean().item(), timestep) # 主引擎標準差
+        # 記錄這批次實際採樣出的動作大小
+        writer.add_scalar('Stats/Action_Main_Mean', old_actions[:, 0].mean().item(), timestep)
 
         # 學習結束後，把新學好的權重同步給 policy_old，作為下一批收集資料時的參考基準。
         self.policy_old.load_state_dict(self.policy.state_dict())
@@ -355,8 +416,8 @@ class PPOAgent:
         storage.clear()
 
         # 傳回平均loss.
-        return sum_loss/self.K_epochs, sum_policy_loss/self.K_epochs, \
-            sum_value_loss/self.K_epochs, sum_entropy/self.K_epochs, sum_advantages/self.K_epochs
+        return sum_loss/loss_calc_count, sum_policy_loss/loss_calc_count, \
+            sum_value_loss/loss_calc_count, sum_entropy/loss_calc_count, sum_advantages/loss_calc_count
 
 class Storage:
     def __init__(self):
@@ -365,16 +426,6 @@ class Storage:
         del self.states[:], self.actions[:], self.logprobs[:], self.rewards[:], self.is_terminals[:], self.values[:]
 
 # --- 訓練主程式 ---
-ENV_NAME = "LunarLanderContinuous-v3"
-MAX_EPISODES = 1000
-UPDATE_TIMESTEP = 2000 # 每累積 2000 步更新一次
-
-MODEL_SAVE_PATH = "dqn_model"
-CHECKPOINT_FILE = "ppo_lunar_lander.pth"
-
-os.makedirs(MODEL_SAVE_PATH, exist_ok=True)
-checkpoint_path = os.path.join(MODEL_SAVE_PATH, CHECKPOINT_FILE)
-
 env = gym.make(ENV_NAME)
 state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.shape[0]
@@ -395,6 +446,22 @@ for episode in range(1, MAX_EPISODES + 1):
         action = agent.select_action(state, storage)
         state, reward, terminated, truncated, _ = env.step(action)
         done = terminated or truncated
+
+        # x : 水平位置, range : -2.5 ... 2.5
+        # height : 垂直高度, range : -2.5 ... 2.5
+        # vel_x : 水平速度, range : -10 ... 10
+        # vel_y : 垂直速度, range : -10 ... 10
+        # angle : 角度, range : -6.2831855 ... 6.2831855 (360度的弧度表示)
+        # angular_velocity : 角速度, range : -10 ... 10
+        # leg_left_contact : 左腳接觸地面
+        # leg_right_contact : 右腳接觸地面
+        x, height, vel_x, vel_y, angle, angular_velocity, leg_left_contact, leg_right_contact = state
+
+        # 獎勵塑造：如果你還在空中(垂直速度 vel_y < 0)，且你有在噴火，給一點點鼓勵
+        if vel_y < 0 and vel_y > -0.1 and action[0] > 0:
+            reward += 0.01
+        if vel_y > 0 and action[0] > 0:
+            reward -= 0.01
         
         storage.rewards.append(reward)
         storage.is_terminals.append(done)   # 記錄這一步是否結束了這一局，這對於計算回報 (Rewards-to-go) 非常重要。
@@ -402,7 +469,7 @@ for episode in range(1, MAX_EPISODES + 1):
         
         # 每累積 UPDATE_TIMESTEP (2000) 步就更新一次 PPO 模型。
         if timestep % UPDATE_TIMESTEP == 0:
-            total_l, pol_l, val_l, ent, adv = agent.update(storage)
+            total_l, pol_l, val_l, ent, adv = agent.update(storage, timestep)
 
             # 以 timestep 作為 X 軸，能精確對應訓練進度
             writer.add_scalar('Loss/Total', total_l, timestep)
